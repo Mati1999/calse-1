a36db6d55e00d2aef00c33b5d01438dae769bbb4
Matias Aguilera <aguileramati50@gmail.com>
2023-12-16 16:02:20 -0300

    Primer commit

---
+++ app.js
@@ -0,0 +1,66 @@
+let apellido = "Aguilera"
+const DNI = 10000000
+// Number   1 , 5 , 7.2 
+// String   "texto" , 'texto'
+
+apellido = 5
+
+//SUMA
+let numero1 = 10
+const numero2 = 5
+
+//   10   +    5
+let resultadoSuma = numero1 + numero2
+
+
+//RESTA
+let numero3 = 10
+const numero4 = 5
+
+//   10   +    5
+let resultadoResta = numero1 - numero2
+
+
+//SUMA DE STRING
+
+let apodo = "Male"
+
+
+
+
+//SUMA DE STRING Y NUMBER
+let numero5 = 5
+let texto = "hola"
+
+let resultado = numero5 + texto // "5hola"
+
+
+//CONSOLE
+
+console.log(resultado)
+console.log(5);
+console.log("Hola mundo!");
+
+//PROMPT
+
+// let nombre = prompt("Ingresa tu nombre")
+
+// console.log(nombre);
+
+//ALERT
+
+// alert("Soy un alert!")
+// alert('Soy un alert2!')
+// alert(5)
+
+
+//EJEMPLO 
+
+const anioDeNacimiento = parseInt(prompt("En que año naciste?"))
+let anioActual = parseInt(prompt("Ingrese el año actual"))
+
+let edad = anioActual - anioDeNacimiento
+
+console.log(edad);
+
+alert('Tu edad es: ' + edad)
